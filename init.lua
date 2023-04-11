c021b14554f7f997167842bedeac55f813f8f00e
Christian Steiner <christian.steiner21@gmail.com>
2023-04-11 21:55:28 +0200

    Add keymap for toggle File-Tree.

--- init.lua
+++ init.lua
@@ -267,6 +267,9 @@ vim.cmd([[
   tnoremap <Esc> <C-\><C-n>
 ]])
 
+-- Nvim-Tree mappings
+vim.keymap.set ('n', '<leader>ft', ":NvimTreeToggle<cr>", {silent = true, noremap = true})
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
