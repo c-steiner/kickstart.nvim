83b0bb674b2feb18cf9feb9cb1983b9a60b854b8
Christian Steiner <christian.steiner21@gmail.com>
2023-04-16 12:13:04 +0200

    Improved generic settings region.

--- init.lua
+++ init.lua
@@ -41,6 +41,8 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Some generic settings
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.cursorline = true
