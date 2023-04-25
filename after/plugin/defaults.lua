1722e47fbf0e6521240e3c6a02edf9f65be7bb85
Christian Steiner <christian.steiner21@gmail.com>
2023-04-25 19:05:00 +0200

    Add key mappings for toggleterm.

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -8,13 +8,10 @@ vim.opt.cp = false
 vim.cmd.colorscheme('nightfly')
 vim.g.nightflyWinSeparator = 2
 
+
 -- Telescope buffer search
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
 
--- Remap terminal behavior
-vim.cmd([[
-  tnoremap <Esc> <C-\><C-n>
-]])
 
 -- toggleterm
 require('toggleterm').setup{
@@ -22,6 +19,20 @@ require('toggleterm').setup{
 }
 
 vim.keymap.set ('n', '<leader>tt', ":ToggleTerm<cr>", {silent = true, noremap = true})
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 
 -- gitui & toggleterm
 vim.keymap.set('n', '<leader>gt', require('utils.term').git_client_toggle, { desc = 'Git TUI' })
